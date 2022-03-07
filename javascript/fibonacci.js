ea3bff0d34bd1b1376a0111add374ae1ebebc508
RyanDeLong <rdelong111@gmail.com>
2022-03-07 13:41:03 -0600

    commit tree 90fc631ec58c215f705f4379e5cd208ec6b4e9c9

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
-  // type your code here
+  let fibList = [0, 1];
+  if (num < 2) {
+    return fibList[num];
+  }
+
+  for (let i = 2; i <= num; i++) {
+    fibList.push(fibList[i - 2] + fibList[i - 1]);
+  }
+
+  return fibList[fibList.length - 1];
 }
 
 if (require.main === module) {
@@ -20,5 +29,21 @@ if (require.main === module) {
 
 module.exports = fibonacci;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/*
+Initialize and set the first two elements of the fibonacci sequence
+If num is less than 2:
+  return the 'num' element of the fibList
+
+Iterate through a loop that completes num - 2 times:
+  Push to fibList the sum of the previous two iteration of fibList
+
+return the last element of fibList
+*/
+
+/*
+Set the first two elements of the fibonacci sequence in array called fibList.
+If num is less than 2, then return fibList[num].
+If num is greater than 2, then set a for loop to iterate until the current wanted 'num' element.
+In the for loop, the current iteration of the fibList is equal to the sum of the previous 2 iterations.
+Once the for loop is complete, return the last element of the fibList.
+*/
